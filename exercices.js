203256ea62458e412f761dbdc700cbbea7c38d6d
SamirGrini <sgrini.dev@gmail.com>
2025-01-22 09:23:47 +0100

    Ajout affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -5,3 +5,13 @@ const nom_magasin = "Archibald Potions Shop";
 let stock_potions_soins = 10;
 let prix_potion = 1;
 let boutique_ouverte = true;
+
+var boutique_ouverte = true; // ou false si la boutique est fermée
+
+if (boutique_ouverte) {
+  console.log("Bienvenue dans la boutique $(nom_magasin) Aventurier !");
+} else {
+  console.log(
+    "La boutique $(nom_magasin) est fermée, revenez plus tard Aventurier !"
+  );
+}
